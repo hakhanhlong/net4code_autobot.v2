79268f347493860e9f466af0bc6f4e980801b2d4
longhk <hakhanhlong@gmail.com>
2017-08-10 18:51:17 +0700

    m

--- runme.py
+++ runme.py
@@ -18,11 +18,11 @@ def main():
 
     stringhelpers.print_welcome()
 
-    '''_mega_manager = MegaManager('MEGA-MANAGEMENT', False)
+    _mega_manager = MegaManager('MEGA-MANAGEMENT', False)
     _mega_manager.start()
 
     _flask_manager = FlaskManager('FLASK-MANAGEMENT', False)
-    _flask_manager.start()'''
+    _flask_manager.start()
 
     _ironman_manager = IronManager('IRONMAN-MANAGEMENT', False)
     _ironman_manager.start()
